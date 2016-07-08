f0b8103668a30ae79fecb20b1ffa1d73e6df5d36
Manwe <gregory.riberon@gmail.com>
2016-07-08 08:25:36 +0200

    add minus and distances methods to Coord class

--- src/competitive/programming/geometry/Coord.java
+++ src/competitive/programming/geometry/Coord.java
@@ -2,9 +2,10 @@ package competitive.programming.geometry;
 
 /**
  * @author Manwe
- * 
- * Class representing a position x y using integers
- * This class contains immutable fields and will return a new instance each time an operation is done
+ *
+ *         Class representing a position x y using integers This class contains
+ *         immutable fields and will return a new instance each time an
+ *         operation is done
  *
  */
 public class Coord {
@@ -13,10 +14,11 @@ public class Coord {
 
     /**
      * Constructor
+     *
      * @param x
-     * 	the x coordinate
+     *            the x coordinate
      * @param y
-     *  the y coordinate
+     *            the y coordinate
      */
     public Coord(int x, int y) {
         this.x = x;
@@ -24,10 +26,11 @@ public class Coord {
     }
 
     /**
-     * Constructor from a vector
-     * A rounding (cast to int) is performed on each x and y doubles of the vector
+     * Constructor from a vector A rounding (cast to int) is performed on each x
+     * and y doubles of the vector
+     *
      * @param v
-     * the vector from which you want to create a coord
+     *            the vector from which you want to create a coord
      */
     public Coord(Vector v) {
         this.x = (int) v.x;
@@ -36,13 +39,49 @@ public class Coord {
 
     /**
      * Add this coord instance to another one to return the sum of the coords
+     *
      * @param coord
-     * 		the other coord to add
-     * @return
-     * 		a new instance with coordinate as the sum of this and the given coord
+     *            the other coord to add
+     * @return a new instance with coordinate as the sum of this and the given
+     *         coord
      */
     public Coord add(Coord coord) {
         return new Coord(x + coord.x, y + coord.y);
+    }
+
+    /**
+     * return the euclidian distance between two coords
+     *
+     * @return sqrt(dx*dx+dy*dy)
+     */
+    public double distance(Coord coord) {
+        return Math.sqrt(distanceSquare(coord));
+    }
+
+    /**
+     * return the euclidian distance square between two coords
+     *
+     * Hint : prefer this square distance if you want to compare distances
+     * rather than the exact distance that cost more
+     *
+     * @return dx*dx+dy*dy
+     */
+    public long distanceSquare(Coord coord) {
+        long dx = Math.abs(coord.x - x);
+        long dy = Math.abs(coord.y - y);
+        return dx * dx + dy * dy;
+    }
+
+    /**
+     * Substract to the instance x and y the values of the given coord
+     *
+     * @param coord
+     *            the other coord to substract
+     * @return a new instance with coordinate as the substraction of this and
+     *         the given coord
+     */
+    public Coord minus(Coord coord) {
+        return new Coord(x - coord.x, y - coord.y);
     }
 
     @Override
--- test/competitive/programming/geometry/CoordTest.java
+++ test/competitive/programming/geometry/CoordTest.java
@@ -1,23 +1,36 @@
 package competitive.programming.geometry;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
-import competitive.programming.geometry.Coord;
-import competitive.programming.geometry.Vector;
-
 public class CoordTest {
 
-	@Test
-	public void add() {
-		Coord coord = new Coord(3,5);
-		
-		assertEquals(new Coord(5,4), coord.add(new Coord(2,-1)));
-	}
+    @Test
+    public void add() {
+        Coord coord = new Coord(3, 5);
 
-	@Test
-	public void convertFromVector(){
-		assertEquals(new Coord(1,2), new Coord(new Vector(1.2, 2.999)));
-	}
+        assertEquals(new Coord(5, 4), coord.add(new Coord(2, -1)));
+    }
+
+    @Test
+    public void distances() {
+        Coord coord = new Coord(3, 5);
+
+        assertEquals(0, coord.distanceSquare(coord));
+        assertEquals(40, coord.distanceSquare(new Coord(1, -1)));
+        assertEquals(5, coord.distance(new Coord(-1, 2)), 0.001);
+    }
+
+    @Test
+    public void minus() {
+        Coord coord = new Coord(3, 5);
+
+        assertEquals(new Coord(1, 6), coord.minus(new Coord(2, -1)));
+    }
+
+    @Test
+    public void convertFromVector() {
+        assertEquals(new Coord(1, 2), new Coord(new Vector(1.2, 2.999)));
+    }
 }
